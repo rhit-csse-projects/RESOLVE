9be2e02c75dedbaf16ceda35eabc8f23b1aeec91
JL <koenigjl@rose-hulman.edu>
2025-01-13 09:52:03 -0500

    added basic command loop

--- src/java/edu/clemson/rsrg/nProver/registryCI/RegistryCI.java
+++ src/java/edu/clemson/rsrg/nProver/registryCI/RegistryCI.java
@@ -2,22 +2,23 @@ package edu.clemson.rsrg.nProver.registryCI;
 
 import edu.clemson.rsrg.nProver.registry.CongruenceClassRegistry;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class RegistryCI {
     private CongruenceClassRegistry registry;
     private Map<String, Integer> symbolToMapping;
     private List<String> mappingToSymbol;
+    private boolean isRunning;
+    private Scanner scanner;
 
-
+    private static final String COMMAND_PROMPT = "> ";
 
     public RegistryCI() {
         registry = new CongruenceClassRegistry<>(100, 100, 100, 100);
         symbolToMapping = new HashMap<String, Integer>();
         mappingToSymbol = new ArrayList<String>();
+        isRunning = true;
+        scanner = new Scanner(System.in);
     }
 
 
@@ -28,17 +29,42 @@ public class RegistryCI {
     public void processCommand(String command, String arg) {
         switch (command) {
             case "Q":
+                isRunning = false;
+                break;
             case "R":
             case "A":
             case "?":
             case "M":
                 System.out.println("UNIMPLEMENTED FUNCTION");
+                break;
             default:
                 System.out.println("Unspecified command: " + command);
         }
     }
 
+    public void runCommandLoop() {
+        sendStartupMessage();
+        while (isRunning) {
+            System.out.print(COMMAND_PROMPT);
+            String input = scanner.nextLine();
+            String[] command = input.split(" ");
+
+            //TODO check this
+            if (command.length > 2) {
+                System.out.println("Max command length is <command> <arg>");
+                continue;
+            }
+
+            if (command.length == 1) {
+                processCommand(command[0], null);
+            } else {
+                processCommand(command[0], command[1]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        RegistryCI registryCI = new RegistryCI();
+        registryCI.runCommandLoop();
     }
 }
